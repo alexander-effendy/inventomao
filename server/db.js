ca4cb9487e8b0fbb15d75f8bb3e1920c171db137
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-06-04 18:04:13 +1000

    added connection string pool in db.js

--- server/db.js
+++ server/db.js
@@ -3,12 +3,23 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
+
+
